d0857d69c6a829402c4950f852aaba34d1027bcc
Scott James <scottjames@scotts-mac-mini.lan>
2021-03-24 06:29:27 -0700

    removed troubling components added to layoutjs

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,34 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import Header from "./Header"
-import Footer from "./Footer"
+import { Link } from "gatsby"
 import "./layout.css"
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Header />
+      <header className=" py-6">
+        <div className="container flex justify-between">
+          <span className="font-black uppercase text-indigo-800">
+            <Link to="/">scott james</Link>
+          </span>
+          <div className="space-x-5">
+            <Link to="/projects" className="capitalize">
+              Projects
+            </Link>
+            <Link to="/contact" className="capitalize">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </header>
 
       <main>{children}</main>
-      <Footer />
+      <footer>
+        <div className="container my-4 font-mono text-center">
+          {" "}
+          © {new Date().getFullYear()}, Scott James
+        </div>
+      </footer>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,10 @@ const IndexPage = () => (
     <SEO title="Home" />
     <section className="container">
       <h1 className="text-3xl font-black my-4">Hi! I'm Scott</h1>
-      <p className="font-mono">
+      <p className="font-mono py-2">
         My purpose is to serve others through digital means. I build landing
-        pages, custom shopify stores, and sales funnels for your projects. Let's
-        get together and discuss your project.
+        pages, custom shopify stores, and sales funnels for your projects. Let'
+        rfsdfs get together and discuss your project.
       </p>
       <button className="flex items-center my-4 px-8 py-4 text-white bg-indigo-800 text-sm rounded-lg hover:bg-indigo-900 transition ease-in-out">
         Get in touch
