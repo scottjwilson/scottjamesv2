e5c2e446be567cccdac7b87c2c17851c10dbc4d0
Scott James <scottjames@scotts-mac-mini.lan>
2021-03-24 06:02:31 -0700

    same error

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,25 +1,13 @@
 import * as React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./Header"
 import Footer from "./Footer"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header />
 
       <main>{children}</main>
       <Footer />
